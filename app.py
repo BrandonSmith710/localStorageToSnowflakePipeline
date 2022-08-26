11109f762435c4bb67dca0711ed5fd31c6df75e0
Brandon Smith <75755695+BrandonSmith710@users.noreply.github.com>
2022-08-25 19:35:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ and inserts the contents of the specified folder into the Snowflake table.
 '''
 
 if __name__ == '__main__':
-	username = input('Enter Snowflake username: ')
-	password = input('Enter Snowflake password: ')
-	account = input('Enter Snowflake account: ')
-	folder_path = input('Enter the filepath for the folder containing csv files for upload: ')
-	qualified_table = input('Enter the qualified table name you would like to store the files in: ')
-	csv_path_list = functions.get_csv_list(folder_path)
-	ctx = functions.get_conn(
-		username = username,
-		password = password,
-		account = account
-		)
-	functions.load_csvs_to_snowflake_table(
-		ctx = ctx,
-		qualified_table = qualified_table,
-		csv_path_list = csv_path_list
-		)
-	print('File upload complete')
+    username = input('Enter Snowflake username: ')
+    password = input('Enter Snowflake password: ')
+    account = input('Enter Snowflake account: ')
+    folder_path = input('Enter the filepath for the folder containing csv files for upload: ')
+    qualified_table = input('Enter the qualified table name you would like to store the files in: ')
+    csv_path_list = functions.get_csv_list(folder_path)
+    ctx = functions.get_conn(
+	username = username,
+	password = password,
+	account = account
+	)
+    functions.load_csvs_to_snowflake_table(
+        ctx = ctx,
+        qualified_table = qualified_table,
+        csv_path_list = csv_path_list
+        )
+    print('File upload complete')
